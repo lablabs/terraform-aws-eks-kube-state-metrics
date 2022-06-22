0608ce0e917f39f37faedfbc6bd7d8da7519c955
Manshubh <manshubh13@gmail.com>
2022-06-22 15:08:38 +0530

    (fix) Default helm_chart_version

--- variables.tf
+++ variables.tf
@@ -22,7 +22,7 @@ variable "helm_chart_name" {
 
 variable "helm_chart_version" {
   type        = string
-  default     = "2.1.4"
+  default     = "2.2.0"
   description = "Version of the Helm chart"
 }
 
