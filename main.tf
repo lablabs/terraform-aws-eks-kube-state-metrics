1787556c3300443c2e020ccc64158508a0ad79b4
Attila Sovak <attila.sovak@gmail.com>
2025-05-19 09:34:59 +0200

    docs(kube-state-metrics): fix description

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 /**
  * # AWS EKS kube state metrics Terraform module
  *
- * A terraform module to deploy a [kube-state-metrics](https://github.com/kubernetes/kube-state-metrics) on Amazon EKS cluster.
+ * A Terraform module to deploy a [kube-state-metrics](https://github.com/kubernetes/kube-state-metrics) on Amazon EKS cluster.
  *
  * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-kube-state-metrics/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-kube-state-metrics/actions/workflows/validate.yaml)
  * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-kube-state-metrics/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-kube-state-metrics/actions/workflows/pre-commit.yml)
